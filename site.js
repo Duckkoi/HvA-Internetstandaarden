955fe869d4015d8efc4b19ec57f632980278e6db
MaidDucko <109869215+Duckkoi@users.noreply.github.com>
2023-12-15 07:58:27 +0100

    Add files via upload

--- site.js
+++ site.js
@@ -3,6 +3,4 @@ function addCoors(e) {
     const x = e.clientX;
     const y = e.clientY;
     e.target.style.backgroundPosition = String((x - window.innerWidth/2) / sensitivity) + "px " + String((y - window.innerHeight/2) / sensitivity) + "px";
-
 }
-document.getElementsByClassName("box")[0].addEventListener("mousemove", (e) => addCoors(e))
